810d35b04c240f6c922f9d98bedb203366824b10
0xScythex0 <arjablc@gmail.com>
2023-04-17 16:38:02 +0545

    cart model added

--- lib/models/cart_provider.dart
+++ lib/models/cart_provider.dart
@@ -4,13 +4,33 @@ class CartItem {
   final String id;
   final String name;
   final int quantity;
-  final double totalPrice;
+  final double price;
 
   CartItem(
       {required this.id,
       required this.name,
       required this.quantity,
-      this.totalPrice = 0});
+      required this.price});
 }
 
-class Cart with ChangeNotifier {}
+class Cart with ChangeNotifier {
+  // ignore: prefer_final_fields
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items => {..._items};
+
+  void addItem(String prodId, double price, String name) {
+    if (items.containsKey(prodId)) {
+      _items.update(
+          prodId,
+          (oldItem) => CartItem(
+              id: oldItem.id,
+              name: oldItem.name,
+              quantity: oldItem.quantity + 1,
+              price: oldItem.price));
+    } else {
+      _items.putIfAbsent(prodId,
+          () => CartItem(id: prodId, name: name, price: price, quantity: 1));
+    }
+  }
+}
