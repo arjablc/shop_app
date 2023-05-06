5ca150ce11ba6c0dc78127d942cbcaf71a80b3aa
Arjablc <arjablc@gmail.com>
2023-05-07 00:11:59 +0545

    config files updates

---
+++ lib/UI/screen/new_product_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NewProductScreen extends StatelessWidget {
+  const NewProductScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('The new products screen'),
+      ),
+    );
+  }
+}
